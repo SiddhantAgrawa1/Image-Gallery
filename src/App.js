db47f1d4b9309dcb8ec04f9ae00350baba672d46
SiddhantAgrawa1 <siddhantagrawal2001@gmail.com>
2022-08-15 23:00:20 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import PhotoAlbum from "react-photo-album";
+import axios from "axios";
+import { useState,useEffect } from "react";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-function App() {
+export default function App() {
+
+  const [Photos,setPhotos] = useState([])
+  const [photo_limit,setPhotoLimit] = useState(30);
+  
+  const fetchMoreData = () => {
+
+    setPhotoLimit(Math.round(Math.random() * 100))
+
+    const page = Math.round(Math.random() * 10)
+    const url = `https://picsum.photos/v2/list?page=${page}&limit=${photo_limit}`
+    console.log(url)
+      axios.get(url).then((response) => 
+      response.data.map(photo => ({src : photo.download_url,width:photo.width,height:photo.height}))
+      ).then((photos) => setPhotos((old_photos) => [...old_photos,...photos]))
+    }
+
+  useEffect( () => {
+    fetchMoreData()
+  },[])
+  //console.log(Photos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <InfiniteScroll
+      dataLength={Photos.length}
+      next={fetchMoreData}
+      hasMore={true}
+      loader={<h4>Loading...</h4>} >
+
+      <PhotoAlbum layout="masonry" photos={Photos} />
+
+    </InfiniteScroll>
   );
 }
-
-export default App;
